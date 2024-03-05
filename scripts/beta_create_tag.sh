0dee43872ba339d157d590896a72b7945fb36a02
j.sharif <jamal.sharif@deliveryhero.com>
2024-03-05 17:38:23 +0100

    adding scripts

---
+++ scripts/beta_create_tag.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -ex
+
+offset=${1:0} # first parameter with default value 0
+year=$(date +%y)
+week=$(date +%V)
+week=$((10#${week} + offset))
+
+if [ $((week % 2)) -eq 0 ]; then #if even week then Week + 1
+  week=$((10#${week} + 1))
+fi
+
+if [[ 10#${week} -gt 52 ]]; then
+  week=1
+  year=$((10#${year} + 1))
+fi
+
+latest_tag=$(git tag -l --sort=-version:refname "v*.*.*" | sed -n "1p")
+IFS='.' read -r -a vers <<<"$latest_tag" # split by dot
+
+major=${vers[0]}
+current_week=${vers[1]:(-2)}
+current_increment=${vers[2]%"-rollback"}
+current_increment=${current_increment%"-beta"}
+
+increment=0
+formattedWeek="$(printf "%02d" $week)"
+
+if [ "$current_week" == "$formattedWeek" ]; then
+  increment=$((10#${current_increment} + 1))
+fi
+
+new_version="${major}.${year}${formattedWeek}.${increment}"
+
+commit_count=$(git rev-list --count origin/develop..HEAD)
+new_version="${new_version}-beta.${commit_count}"
+echo "New version number is: $new_version"
+
+git tag -a "$new_version" -m "$new_version"
+git push origin "$new_version"
---
+++ scripts/beta_trigger.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -ex
+
+echo "beta was triggered"
+git status
